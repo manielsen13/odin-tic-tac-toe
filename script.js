0e7bc1958e4e880a3817122cb4c93e396642c5e4
manielsen13 <marshall.niels@gmail.com>
2022-10-19 21:59:07 -0400

    Make Space Factory.

---
+++ script.js
@@ -0,0 +1,62 @@
+//create space factory
+function spaceFactory() {
+
+    const mark = "";
+
+    const isFilled = () => {
+        if (mark != "") {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    const markSpace = (symbol) => {
+        mark = symbol;
+    }
+}
+
+
+
+
+//creat board module
+function board(){
+    const spaces = [9];
+    
+}
+
+//create user module
+function user() {
+
+}
+
+//create computer/AI module
+function computerPlayer {
+
+}
+
+//create game module
+
+
+//factory example
+const personFactory = (name, age) => {
+    const sayHello = () => console.log('hello!');
+    return { name, age, sayHello };
+  };
+
+//module example
+const calculator = (() => {
+    const add = (a, b) => a + b;
+    const sub = (a, b) => a - b;
+    const mul = (a, b) => a * b;
+    const div = (a, b) => a / b;
+    return {
+      add,
+      sub,
+      mul,
+      div,
+    };
+  })();
+  
+  calculator.add(3,5); // 8
+  calculator.sub(6,2); // 4
+  calculator.mul(14,5534); // 77476
